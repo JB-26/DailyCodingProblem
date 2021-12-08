4fcc0c079d2da778a4124691e8fa5ea13b2ddbd0
Joshua Blewitt <joshblewitt@protonmail.com>
2021-12-08 22:11:15 +0000

    Init commit for next problem!

---
+++ cheapest_itinerary.py
@@ -0,0 +1,46 @@
+# THE PROBLEM
+# You are given a huge list of airline ticket prices between cities around the world on a given day.
+# These are all direct flights
+# Each element in the list has the format (source_city, destination, price)
+# Consider a user who is willing to take up k connections to get from their origin city A to destination B.
+# Find the cheapest fare possible for this journey and print the itinerary.
+
+# For example, a traveler wants to go from JFK to LAX with up to 3 connections, with the following input
+
+# [
+    # ('JFK', 'ATL', 150),
+    # ('ATL', 'SFO', 400),
+    # ('ORD', 'LAX', 200),
+    # ('LAX', 'DFW', 80),
+    # ('JFK', 'HKG', 800).
+    # ('ATL', 'ORD', 90),
+    # ('JFK', 'LAX', 500 )
+# ]
+
+# This would return JFK -> ATL -> ORD -> LAX, costing 440
+
+# import libraries
+import heapq
+
+from collections import defaultdict
+
+# create list of flights (tuples)
+flights = [('JFK', 'ATL', 150), ('ATL', 'SFO', 400), ('ORD', 'LAX', 200), ('LAX', 'DFW', 80), ('JFK', 'HKG', 800), ('ATL', 'ORD', 90), ('JFK', 'LAX', 500 )]
+
+def get_itinerary(flights, source, destination, k):
+    """
+    Returns the cheapest fare possible for a given journey
+    """
+
+    print(f"Flights for today:\n")
+    for a, b, c in flights:
+        print(f"Source: {a} Destination: {b} Cost: {c}")
+
+    print(f"The start of the journey is {source}")
+
+    print(f"The destination is {destination}")
+
+    print(f"The maximum ammount of connections is {k}")
+
+
+get_itinerary(flights, 'JFK', 'LAX', 3)
